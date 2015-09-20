6ffc5548e065425d6d51e6da0f1c8492edde41de
Danielle Giglia <daniellegiglia@yahoo.com>
2015-09-20 02:51:33 +0000

    test for validations on videos

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,20 @@ describe Video do
     simpsons = Video.create(title: "The Simpsons", description: "Doh!", category: comedy)
     expect(simpsons.category).to eq(comedy)
   end
+  
+  it "does not save video without title" do
+    Video.create(description: "super funny show")
+    expect(Video.count).to eq(0)
+  end
+  
+  it "does not save video without description" do
+    Video.create(title: "That 70s Show")
+    expect(Video.count).to eq(0)
+  end
+  
+  it "does not save video with duplicate title" do
+    Video.create(title: "Mork and Mindy", description: "Robin Williams is an alien!")
+    Video.create(title: "Mork and Mindy", description: "Classic comedy")
+    expect(Video.count).to eq(1)
+  end
 end
