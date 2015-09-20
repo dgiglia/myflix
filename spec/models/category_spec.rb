68c0c56f2b2632bede1bc9010c3168bec9864c3b
Danielle Giglia <daniellegiglia@yahoo.com>
2015-09-20 03:05:03 +0000

    installed should-matchers gem, refactored video and category tests accordingly

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,5 @@
 require "spec_helper"
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: "Drama")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-  
-  it "has many videos" do
-    drama = Category.create(name: "Drama")
-    turn = Video.create(title: "Turn", description: "Revolutionary war era drama", category: drama)
-    fosters = Video.create(title: "The Fosters", description: "Modern day foster family", category: drama)
-    expect(drama.videos).to eq([fosters, turn])
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,31 +1,8 @@
 require "spec_helper"
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Jericho", description: "American post-apocalyptic action-drama series")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-  
-  it "belongs to category" do
-    comedy = Category.create(name: "Comedy")
-    simpsons = Video.create(title: "The Simpsons", description: "Doh!", category: comedy)
-    expect(simpsons.category).to eq(comedy)
-  end
-  
-  it "does not save video without title" do
-    Video.create(description: "super funny show")
-    expect(Video.count).to eq(0)
-  end
-  
-  it "does not save video without description" do
-    Video.create(title: "That 70s Show")
-    expect(Video.count).to eq(0)
-  end
-  
-  it "does not save video with duplicate title" do
-    Video.create(title: "Mork and Mindy", description: "Robin Williams is an alien!")
-    Video.create(title: "Mork and Mindy", description: "Classic comedy")
-    expect(Video.count).to eq(1)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_uniqueness_of(:title) }
 end
