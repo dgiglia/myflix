65e9a7463dc799947c8788c1d6fead78f06d9011
Danielle Giglia <daniellegiglia@yahoo.com>
2015-09-20 14:34:20 +0000

    Fixed one liner should syntax to expect syntax on specs

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe Video do
-  it { should belong_to(:category) }
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
-  it { should validate_uniqueness_of(:title) }
+  it { is_expected.to belong_to(:category) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_uniqueness_of(:title) }
 end
