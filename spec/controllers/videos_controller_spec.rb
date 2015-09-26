335932e1d26a0d0a946f43f2a902482dc3617d02
Danielle Giglia <daniellegiglia@yahoo.com>
2015-09-26 20:16:05 +0000

    Test for video controller show and search actions

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe "GET show" do
+    it "sets the @video for authenticated user" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+    
+    it "redirects unauthenticated user to front page" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to root_path
+    end
+  end
+  
+  describe "GET search" do
+    it "sets @results for authenticated user" do
+      session[:user_id] = Fabricate(:user).id
+      dr_who = Fabricate(:video, title: 'Doctor Who')
+      get :search, search_term: 'doc'
+      expect(assigns(:results)).to eq([dr_who])
+    end
+    
+    it "redirects unauthenticated user to front page" do
+      dr_who = Fabricate(:video, title: 'Doctor Who')
+      get :search, search_term: 'doc'
+      expect(response).to redirect_to root_path
+    end
+  end
+
+end
