4ed4dedeff5999a72005891bf5fbb6ee09e637ff
Danielle Giglia <daniellegiglia@yahoo.com>
2015-10-25 18:58:27 +0000

    gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def rating_options(selected=nil)
     options_for_select((1..5).map { |rating| [pluralize(rating, "Star"), rating]}, selected)
   end
+  
+  def gravatar_for(user)
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}?s=40"
+  end
 end
