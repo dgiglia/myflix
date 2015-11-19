38852cb309ca7b9a9562ac54af34173c555e6247
Danielle Giglia <daniellegiglia@yahoo.com>
2015-11-19 02:47:10 +0000

    Implement User Signup Service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,24 +7,12 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.valid?                 
-      charge = StripeWrapper::Charge.create(
-        amount: 999,
-        card: params[:stripeToken],
-        description: "MyFlix Sign Up Charge for #{@user.email}"
-      )
-      if charge.successful?
-        @user.save     
-        handle_invitation
-        AppMailer.delay.send_welcome_email(@user)
-        flash['success'] = "Profile was successfully created. Please sign in."
-        redirect_to sign_in_path
-      else
-        flash.now['danger'] = charge.error_message
-        render :new
-      end
-    else
-      flash.now['danger'] = "Invalid user information. Please check error messages below."
+    result = UserSignup.new(@user).sign_up(params[:stripeToken], params[:invitation_token])
+    if result.successful?
+      flash['success'] = "Profile was successfully created. Please sign in."
+      redirect_to sign_in_path
+    else 
+      flash.now['danger'] = result.error_message
       render :new
     end
   end
@@ -47,14 +35,5 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :password, :email)
-  end
-  
-  def handle_invitation
-    if params[:invitation_token].present?
-      invitation = Invitation.find_by(token: params[:invitation_token])
-      @user.follow(invitation.inviter)
-      invitation.inviter.follow(@user)
-      invitation.update_column(:token, nil)
-    end
-  end    
+  end      
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,102 +8,31 @@ describe UsersController do
     end
   end
   
+
+  
   describe "POST create" do
-    context "sending emails" do
-      after {ActionMailer::Base.deliveries.clear}
-      let(:charge) {double(:charge, successful?: true)}
+    context "with successful sign up" do
+      let(:result) {double(:sign_up_result, successful?: true)}  
       before do
-        ActionMailer::Base.deliveries.clear
-        allow(StripeWrapper::Charge).to receive(:create).and_return(charge)
-      end
-      
-      it "sends out email to the user with valid inputs" do
-        post :create, user: {email: "john@example.com", password: "password", name: "john smith"}
-        expect(ActionMailer::Base.deliveries.last.to).to eq(["john@example.com"])
-      end
-      
-      it "sends out email containg the user's name with valid inputs" do
-        post :create, user: {email: "john@example.com", password: "password", name: "john smith"}
-        expect(ActionMailer::Base.deliveries.last.body).to include("john smith")
-      end
-      
-      it "does not send out email with invalid inputs" do
-        post :create, user: {email: "john@example.com", name: "john smith"}
-        expect(ActionMailer::Base.deliveries).to be_empty
-      end
-    end
-    
-    context "with valid personal info and valid card" do
-      let(:charge) {double(:charge, successful?: true)}
-      before do
-        expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
         post :create, user: Fabricate.attributes_for(:user)
       end
-      
-      it "creates the user" do        
-        expect(User.count).to eq(1)
-      end
-      
+      it {is_expected.to set_flash[:success]}
       it {is_expected.to redirect_to sign_in_path}         
     end    
     
-    context "with valid personal info and declined card" do
-      let(:charge) {double(:charge, successful?: false, error_message: "Your card was declined.")}
+    context "with failed sign up" do
+      let(:result) {double(:sign_up_result, successful?: false, error_message: "Your card was declined.")}
       before do
-        expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
-        post :create, user: Fabricate.attributes_for(:user), stripeToken: '123123'
-      end
-      
-      it "does not create a new user record" do
-        expect(User.count).to eq(0)
-      end
-      
-      it {is_expected.to render_template(:new)}
-      
-      it {is_expected.to set_flash.now[:danger]}
-    end    
-    
-    context "with invalid personal info" do
-      before {post :create, user: {password: "password", name: "your name"}}
-      
-      it "does not create user" do        
-        expect(User.count).to eq(0)
-      end
-      
-      it {is_expected.to render_template(:new)} 
-      
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
+        post :create, user: Fabricate.attributes_for(:user)
+      end      
       it "sets @user" do
         expect(assigns(:user)).to be_instance_of(User)
       end
-      
-      it "does not charge the card" do
-        expect(StripeWrapper::Charge).not_to receive(:create)
-      end
-    end
-    
-    context "with valid personal info, valid card, and invitation" do
-      let(:dean) {Fabricate(:user)}
-      let(:invitation) {Fabricate(:invitation, inviter: dean, recipient_email: "sam@example.com")}
-      let(:sam) {User.find_by(email: "sam@example.com")}
-      let(:charge) {double(:charge, successful?: true)}
-      before do
-        expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
-        post :create, user: {email: "sam@example.com", password: "password", name: "sam winchester"}, invitation_token: invitation.token
-      end
-      after {ActionMailer::Base.deliveries.clear}
-      
-      it "makes the user follow the inviter" do        
-        expect(sam.follows?(dean)).to be true
-      end
-      
-      it "makes the inviter follow the user" do
-        expect(dean.follows?(sam)).to be true
-      end
-      
-      it "expires the invitation upon acceptance" do
-        expect(Invitation.first.token).to be nil
-      end
-    end    
+      it {is_expected.to render_template(:new)}      
+      it {is_expected.to set_flash.now[:danger]}
+    end       
   end
   
   describe "GET show" do
